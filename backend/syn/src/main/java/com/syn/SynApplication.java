7631bc28dd7d52e57447cb13306fcad2859ff011
Esther Ferreira <estherferreira.cm@gmail.com>
2023-09-26 21:13:06 -0300

    initial commit

---
+++ backend/syn/src/main/java/com/syn/SynApplication.java
@@ -0,0 +1,13 @@
+package com.syn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynApplication.class, args);
+	}
+
+}
