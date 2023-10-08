1653505509a1d75cc9e0b373495c66a78cb91f8c
Esther Ferreira <estherferreira.cm@gmail.com>
2023-10-08 10:39:19 -0300

    feat: implement and test the User class

--- backend/syn/src/main/java/com/syn/SynApplication.java
+++ backend/syn/src/main/java/com/syn/SynApplication.java
@@ -2,12 +2,21 @@ package com.syn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.syn.models.User;
 
 @SpringBootApplication
 public class SynApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SynApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SynApplication.class, args);
 
+        try {
+            // Crie o objeto User usando o construtor
+            User user = new User("luanapiovani@gmail.com", "LuPiovani#12", "Luana Piovani", (byte) 1, (byte) 1, (short) 2001, "A-");
+            System.out.print(user);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
