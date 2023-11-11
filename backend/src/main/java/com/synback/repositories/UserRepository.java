15b47e98c88671dfa9f7c5251c87ebbcd84fc665
Esther Ferreira <estherferreira.cm@gmail.com>
2023-11-11 13:58:34 -0300

    refactor: remove files

--- backend/src/main/java/com/synback/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.synback.repositories;
-
-import com.synback.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
