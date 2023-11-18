3a34c5a0633e6117ad5ab5192b5af5d802ac3cef
Esther Ferreira <estherferreira.cm@gmail.com>
2023-11-18 09:50:33 -0300

    refactor: create 'AuthenticationUserRepository' repository

--- backend/src/main/java/com/synback/repositories/AuthenticationUserRepository.java
+++ backend/src/main/java/com/synback/repositories/AuthenticationUserRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthenticationUserRepository extends
-GenericRepository<AuthenticationUser, String> {
+GenericRepository<AuthenticationUser, Integer> {
 AuthenticationUser findByEmail(String email);
 }
